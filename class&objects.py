1c130336957c3efd2a98928514e549fcb627a628
MukeshPal <1203mukeshpal@gmail.com>
2024-10-15 11:58:15 +0530

    Code has been published with some changes.

--- class&objects.py
+++ class&objects.py
@@ -15,7 +15,7 @@ class Bank:
         amount = int(input("enter the deposit amount : "))
         self.balance += amount
 
-    def withraw(self):  
+    def withdraw(self):  
         amount = int(input("enter the deposit amount : "))
         if amount > self.balance:
             print(f"Insufficient balance : {self.balance}")
@@ -25,12 +25,13 @@ class Bank:
 
     
     def get_info(self):
-        return f"Hello my name is {self.name} and My account no is {self.account}. The amount in my account is {self.amount}.and I can withdraw amount using my phone no {self.phone}"
+        return f"Hello my name is {self.name} and My account no is {self.account}. The amount in my account is {self.balance}.and I can withdraw amount using my phone no {self.phone}"
 
 Banks = []   
 while True:
     print("1. create Bank account")
     print("2. Show all Bank details")
+    print("3. enter amount to deposit")
     print("3. exit")
     choice = int(input("enter the choice"))
 
@@ -40,6 +41,30 @@ while True:
     elif choice == 2 :
         if len(Banks)==0:
             print("no account is created yet")
+        else:
+            for bank in Banks:
+                print(bank.get_info())
+
+    elif choice == 3:
+        account = int(input("enter the account no. to deposit : "))
+    
+        for bank in Banks:
+            if bank.account == account:
+                bank.deposit()
+
+    elif choice == 4:
+        account = int(input("enter the account no. to deposit : "))
+    
+        for bank in Banks:
+            if bank.account == account:
+                bank.withdraw()
+                break
+
+    elif choice == 5:
+        break
+
+    else:
+        print("invalid choice")
     
 
 
